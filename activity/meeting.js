8a258f61bbef60e35e080105ab722da40b1b1a2f
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-04-18 17:43:37 +0530

    Updated meeting details

--- activity/meeting.js
+++ activity/meeting.js
@@ -1,27 +1,27 @@
 module.exports = {
     meeting: [{
             startTime: "10:00",
-            amOrPm: "AM",
+            amOrPm: "PM",
             durationHour: "4",
             durationMin: "00",
-            topic: "PAB"
-
+            topic: "PAB",
+            batchEmail: 'shivamgupta9118551@gmail.com'
         },
         {
             startTime: "3:00",
             amOrPm: "PM",
             durationHour: "4",
             durationMin: "00",
-            topic: "PP Batch - 2"
-
+            topic: "PP Batch - 2",
+            batchEmail: 'amulyarock9@gmail.com'
         },
         {
             startTime: "7:00",
             amOrPm: "PM",
             durationHour: "4",
             durationMin: "00",
-            topic: "PP Batch - 1"
-
+            topic: "PP Batch - 1",
+            batchEmail: 'ajaygupta57121@gmail.com'
         }
     ]
 }
